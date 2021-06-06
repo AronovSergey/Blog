5f500355313eb097840897ddf7f5085232d38217
SergeyAronov <sergey.aronov@sergata.com>
2021-06-06 23:41:30 +0300

    added auth module, added login endpoint and jwt  password hash method

--- api/src/user/service/user.service.ts
+++ api/src/user/service/user.service.ts
@@ -1,27 +1,60 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { from, Observable } from 'rxjs';
+import { from, Observable, throwError } from 'rxjs';
 import { Repository } from 'typeorm';
+import { switchMap, map, catchError } from 'rxjs/operators'
 
 import { UserEntity } from '../models/user.entity';
 import { User } from '../models/user.interface';
+import { AuthService } from '../../auth/services/auth.service';
 
 @Injectable()
 export class UserService {
     constructor(
-        @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>
+        @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+        private authService: AuthService
     ) { }
 
     create(user: User): Observable<User> {
-        return from(this.userRepository.save(user));
+        return this.authService.hashPassword(user.password).pipe(
+            switchMap((password: string) => {
+                const newUser = new UserEntity();
+                newUser.name = user.name;
+                newUser.username = user.username;
+                newUser.email = user.email;
+                newUser.password = password;
+
+                return from(this.userRepository.save(newUser)).pipe(
+                    map((user: User) => {
+                        const { password, ...result } = user;
+                        return result;
+                    }),
+                    catchError(err => throwError(err))
+                )
+            })
+
+        )
+
     }
 
     findOne(id: number): Observable<User> {
-        return from(this.userRepository.findOne({ id }))
+        return from(this.userRepository.findOne({ id })).pipe(
+            map((user: User) => {
+                const { password, ...result } = user;
+                return result;
+            })
+        )
     }
 
     findAll(): Observable<User[]> {
-        return from(this.userRepository.find());
+        return from(this.userRepository.find()).pipe(
+            map((users: User[]) => {
+                return users.map((user: User) => {
+                    delete user.password;
+                    return user;
+                })
+            })
+        );
     }
 
     deleteOne(id: number): Observable<any> {
@@ -29,6 +62,36 @@ export class UserService {
     }
 
     updateOne(id: number, user: User): Observable<any> {
+        delete user.password;
+        delete user.email;
         return from(this.userRepository.update(id, user));
+    }
+
+    login(user: User): Observable<string> {
+        return this.validateUser(user.email, user.password).pipe(
+            switchMap((user: User) => {
+                return this.authService.generateJWT(user).pipe(map((jwt: string) => jwt));
+            })
+        )
+    }
+
+    validateUser(email: string, password: string): Observable<User> {
+        return this.findByMail(email).pipe(
+            switchMap((user: User) => this.authService.comparePasswords(password, user.password).pipe(
+                map((match: boolean) => {
+                    if (match) {
+                        const { password, ...result } = user;
+                        return result;
+                    }
+                    else {
+                        throw Error('Wrong Credentials');
+                    }
+                })
+            ))
+        )
+    }
+
+    findByMail(email: string): Observable<User> {
+        return from(this.userRepository.findOne({ email }))
     }
 }
--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -4,10 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
 import { UserEntity } from './models/user.entity'
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([UserEntity])
+    TypeOrmModule.forFeature([UserEntity]),
+    AuthModule
   ],
   providers: [UserService],
   controllers: [UserController]
